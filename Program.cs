56ce2c100e273ae65d1aa08ba3b58b46dc26be75
turospista <66325842+turospista@users.noreply.github.com>
2020-06-02 22:19:03 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,21 +10,39 @@ namespace pia
             Console.WriteLine("1 - Sör\n2 - Bor\n3 - Pálinka");
 
             int pia = Convert.ToInt32(Console.ReadLine());
+            int sör = 0;
+            int bor = 0;
+            int pálinka = 0;
 
-            if (pia == 1)
+            do
             {
-                Console.WriteLine("A sör finom és kevés benne az alkohol.");
-            }
-            else if (pia == 2)
-            {
-                Console.WriteLine("A bor finom és nem olyan kevés benne az alkohol sem.");
-            }
-            else
-            {
-                Console.WriteLine("A pálinka ütős pia.");
-            }
-        }
-            
-            
+                if (pia == 1)
+                {
+                    Console.WriteLine("A sör finom és kevés benne az alkohol.");
+                    sör++;
+                }
+                else if (pia == 2)
+                {
+                    Console.WriteLine("A bor finom és nem olyan kevés benne az alkohol sem.");
+                    bor++;
+                }
+                else
+                {
+                    Console.WriteLine("A pálinka ütős pia. És finom is.");
+                    pálinka++;
+                }
+
+                Console.WriteLine("Ittál még valamit?");
+
+             
+
+            } while (Console.ReadLine() != "q");
+
+            Console.WriteLine("Ennyi sört ittál: " + sör);
+            Console.WriteLine("Ennyi bort ittál: " + bor);
+            Console.WriteLine("Ennyi pálinka ittál: " + pálinka);
+
+
+        }          
     }
 }
