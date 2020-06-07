5709369ec53988fb6e1a8d793900ac20e01b86f5
turospista <66325842+turospista@users.noreply.github.com>
2020-06-07 14:20:13 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace pia
         public static void Main(string[] args)
         {
             Console.WriteLine("Mit ittál ma?");
-            Console.WriteLine("Sört, bort vagy pálinkát?");
+            Console.WriteLine("Sört (5%, 5 dl), bort (8%, 3 dl) vagy pálinkát (48%, 0,4 dl)?");
 
             int sör = 0;
             int bor = 0;
@@ -17,6 +17,7 @@ namespace pia
 
             do
             {
+
                 string pia = Console.ReadLine();
 
                 if (pia != "sört" && pia != "bort" && pia != "pálinkát")
@@ -28,7 +29,7 @@ namespace pia
                 if (pia == "sört")
                 {
                     Console.WriteLine("A sör finom.");
-                    sör++;
+                    sör++; 
                 }
 
 
@@ -54,10 +55,41 @@ namespace pia
             Console.WriteLine("Ennyi bort ittál: " + bor);
             Console.WriteLine("Ennyi pálinkát ittál: " + pálinka);
 
-            Console.WriteLine("Megérte??\nKilépéshez nyomj 'q' betűt.");
+            double sormennyiseg = sör * 5; // dl
+            double bormennyiseg = bor * 3; // dl
+            double palinkamennyiseg = pálinka * 0.4; // dl
 
+
+            // alkohol (g) = ital (dl) x V/V% (vol %) x 0,8
+            // vérezrelék számítás: vérezrelék = alkohol (g) / Ts (kg)
+            // Példával bemutatva. 5 dl, 4 V/V%-os sör elfogyasztása 5 x 4 x 0,8 = 16 g etil-alkoholnak felel meg.
+            // Ha ezt a mennyiséget egy 70 kg - os ember fogyasztja el: 16 / 70 = 0,2 ezreléknek felel meg.
+            // lebontás: 0,15 ‰ / óra
+
+            Console.WriteLine("Hány kg vagy?");
+            int testsuly = Convert.ToInt32(Console.ReadLine());
+
+            double soralkoholmennyiseg = sormennyiseg * 5 * 0.8;
+            double boralkoholmennyiseg = bormennyiseg * 8 * 0.8;
+            double palinkaalkoholmennyiseg = palinkamennyiseg * 48 * 0.8;
+
+            double osszalkoholmennyiseg = soralkoholmennyiseg + boralkoholmennyiseg + palinkaalkoholmennyiseg;
+
+            double verezrelek = osszalkoholmennyiseg / testsuly;
+            verezrelek = Math.Round(verezrelek, 2);
+
+            Console.WriteLine("Az alkohol vérezrelék szinted: " + verezrelek + "‰");
+            double lebontas = verezrelek / 0.15;
+            lebontas = Math.Round(lebontas, 2);
+            Console.WriteLine("Még " + lebontas + "óráig ne vezess.");
+
+            Console.WriteLine("Kilépéshez nyomj 'q' betűt.");
             while (Console.ReadKey().Key != ConsoleKey.Q) { }
 
-        }          
+            
+
+
+
+        }
     }
 }
